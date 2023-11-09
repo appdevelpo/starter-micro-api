ff01c652c748dfe5276ff7fe60f81605802ed60e
appdevelpo <56633229+appdevelpo@users.noreply.github.com>
2023-11-09 22:12:25 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
